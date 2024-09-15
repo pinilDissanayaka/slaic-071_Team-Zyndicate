2e05e8bc5ae00db5fe465bfee5fe4787a7a18975
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-09-15 23:46:29 +0530

    add compare node

--- app/utils/comparator.py
+++ app/utils/comparator.py
@@ -18,7 +18,9 @@ class Graph_State(TypedDict):
   
 
 def retrieve_node(state:Graph_State):
-  question=f"Compare the manifestos of {state['candidates']} focusing on the key issues of {state['domain']}"
+  question=f"""Retrieve relevant documents from the vector database based on the following inputs: {state['candidates']} and a 
+  selected category {state['domain']}. Ensure the documents focus on the selected category, providing insights 
+  into the policies or statements made by the specified candidates or parties."""
 
   retrieved_documents=retriever.invoke(question)
 
@@ -26,13 +28,14 @@ def retrieve_node(state:Graph_State):
 
 
 def generate_node(state:Graph_State):
-  summary_prompt_template='''
-        Compare the manifestos of all candidates focusing on the key issues of given : {DOMAIN} with candidate names.
-        using only given context : {CONTEXT}
-        Provide a side-by-side breakdown of their promises, highlighting similarities, differences, and unique approaches.
-        Summarize the main goals and policies for each candidate and offer an analysis of how these align with their past statements or voting records,
-        If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer."
-    '''
+  summary_prompt_template="""You are an assistant tasked with comparing election candidates or political parties based on specific categories. 
+  The user will provide the category {DOMAIN} and the context want to compare. {CONTEXT}
+  Your job is to present a clear, fact-based comparison of how each candidate or party addresses the selected category. 
+  Be objective and highlight the key points from each manifesto or policy statement. 
+  Ensure the comparison is concise and provides meaningful insights for the user to evaluate.
+  Summarize the main goals and policies for each candidate and offer an analysis of how these align with their past statements or voting records,
+  If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+  """
 
   summary_prompt=ChatPromptTemplate.from_template(summary_prompt_template)
 
@@ -52,12 +55,13 @@ def generate_node(state:Graph_State):
 
 
 def evaluate_node(state:Graph_State):
-  evaluate_prompt_template='''
-  Evaluate the manifesto of all candidate on a scale of 1 to 10 based on the following criteria Using given context : {CONTEXT} in the given : {DOMAIN}
-    please provide the score with candidate name ,separate scores using \n.
-    If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
-    Don't try to calculate average score.
-  '''
+  evaluate_prompt_template="""
+  You are an evaluator tasked with reviewing the comparison of given context : {CONTEXT} based on a specific category {DOMAIN}. 
+  Provide a score out of 10 for each candidate or party, 
+  based on how well their manifesto or policy addresses the selected category. Justify the score with a brief explanation, 
+  highlighting strengths and weaknesses in each candidate's or party's approach.
+  If the answer is not found in the context, kindly state "I don't know." Don't try to make up an answer.
+  """
 
   evaluate_prompt=ChatPromptTemplate.from_template(evaluate_prompt_template)
 
