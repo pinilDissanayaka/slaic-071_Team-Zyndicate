1811cc52883abe8208c9cf0101ff7a39dd8fa50c
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-09 01:35:53 +0530

    add pyplot

--- app/pages/💡 Manifesto Matchmaker.py
+++ app/pages/💡 Manifesto Matchmaker.py
@@ -2,7 +2,7 @@ import os
 import streamlit as st
 from utils.utils import save_pdf_txt_on_temp_dir, load_into_vector_store, stream_text
 from utils.manifestomatchmaker import get_relevant_policies
-from utils.alignpolicy import get_align_candidate, draw_pie_plot
+from utils.alignpolicy import get_align_candidate
 
 selected_policies=[]
 list_of_policies=[]
@@ -52,10 +52,9 @@ if list_of_policies:
 t=st.text_input("enter")
 
 if t:
-    #st.write(get_align_candidate(t))
+    st.write(get_align_candidate(t))
     
-    st.pyplot(draw_pie_plot(["a", "b"], [0.5, 0.5])
-)
+    st.pyplot(get_align_candidate(t)[-1])
 
 
 
