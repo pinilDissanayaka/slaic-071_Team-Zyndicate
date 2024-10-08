d2f900b75519957085194d2e27f97198d740e588
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-09 01:54:13 +0530

    add pyplot

--- app/utils/alignpolicy.py
+++ app/utils/alignpolicy.py
@@ -75,7 +75,7 @@ graph=workflow.compile()
 
 def draw_pie_plot(labels, sizes):
     fig, ax=plt.subplots()
-    ax.pie(sizes, labels=labels, autopct='%1.1f%%', shadow=True)
+    ax.pie(sizes, labels=labels, autopct='%1.12f%%', shadow=True)
     ax.axis('equal')
     
     return fig
