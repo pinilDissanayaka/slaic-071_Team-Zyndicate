d7fb14f80484e9008675aee22a552f30607e9f88
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-09 21:35:32 +0530

    change search k

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -27,7 +27,7 @@ llm_model="llama3-groq-70b-8192-tool-use-preview"
 vision_model="llava-v1.5-7b-4096-preview"
 embedding_model="models/text-embedding-004"
 vector_store_index_name="manifesto"
-search_k=4
+search_k=5
 temperature=0.9
 temp_dir="temp"
 
