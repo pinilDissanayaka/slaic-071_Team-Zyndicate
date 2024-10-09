f3f4a4cc061359c52ce239c83cba346e807a6134
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-09 13:30:22 +0530

    add tempurature

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -27,7 +27,8 @@ llm_model="llama3-groq-70b-8192-tool-use-preview"
 vision_model="llava-v1.5-7b-4096-preview"
 embedding_model="models/text-embedding-004"
 vector_store_index_name="manifesto"
-search_k=5
+search_k=7
+temperature=0.4
 temp_dir="temp"
 
 
@@ -37,7 +38,7 @@ retriever=PineconeVectorStore(embedding=embeddings, index_name=vector_store_inde
 
 
 llm=ChatGroq(model=llm_model,
-            temperature=0.01,
+            temperature=temperature,
             max_tokens=None,
             timeout=None)
 
