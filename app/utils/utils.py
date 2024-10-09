3ac9d23d46eab76df3b6c3a2e34915d3644b7ee4
pinilDissanayaka <pinildissanayaka@gmail.com>
2024-10-09 12:52:34 +0530

    chnage tempurature

--- app/utils/utils.py
+++ app/utils/utils.py
@@ -37,7 +37,7 @@ retriever=PineconeVectorStore(embedding=embeddings, index_name=vector_store_inde
 
 
 llm=ChatGroq(model=llm_model,
-            temperature=0.65,
+            temperature=0.01,
             max_tokens=None,
             timeout=None)
 
