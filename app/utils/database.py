e96fead9538fdd53be85a99110ff0059df3059f1
Denuwan-Kalubowila <denuwankalubowila@gmail.com>
2024-10-11 08:40:16 +0530

    update facts

--- app/utils/database.py
+++ app/utils/database.py
@@ -1,22 +1,24 @@
+import os
 from langchain_community.utilities import SQLDatabase
 from langchain_community.agent_toolkits import create_sql_agent
+from langchain_community.agent_toolkits import SQLDatabaseToolkit
 from sqlalchemy.exc import SQLAlchemyError
-from utils.utils import get_llm
+from langchain_groq.chat_models import ChatGroq
+from langchain_google_genai import GoogleGenerativeAI
+from utils import get_llm
 
-# Create the SQLDatabase object
+
+
 db = SQLDatabase.from_uri("postgresql://Denuwan:rsqY2Jbj7CZQ@ep-dark-frog-a5hpd82n.us-east-2.aws.neon.tech/Election?sslmode=require")
 
-# Print the SQL dialect being used (optional)
 print("Dialect:", db.dialect)
 
-# Get table names
 try:
     tables = db.get_usable_table_names()
     print("Tables:", tables)
 except SQLAlchemyError as e:
     print(f"Error fetching table names: {e}")
 
-# Run the query
 try:
     result = db.run("SELECT * FROM candidates LIMIT 10;")
     print("Query Result:", result)
@@ -24,7 +26,10 @@ except SQLAlchemyError as e:
     print(f"Query Execution Error: {e}")
 
 llm = get_llm()
-agent_executor = create_sql_agent(llm, db=db, agent_type="openai-tools", verbose=True)
-res = agent_executor.invoke(
-    "List the total sales per country. Which country's customers spent the most?"
+tool_kit = SQLDatabaseToolkit(
+    db=db,
+    llm=llm
 )
+agent_executor = create_sql_agent(llm, toolkit=tool_kit, verbose=True)
+res=agent_executor.invoke("List the candidates who nominated from ratnapura ",handle_parsing_errors=True)
+print(res)
